468b3e2be2f1a984389a38cfa04b390891172543
trela_1103395 <trela_1103395@a05>
2017-10-02 13:23:14 +0200

    add tulip class

---
+++ Kwiaciarnia/src/kwiaciarnia/tulip.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kwiaciarnia;
+
+/**
+ *
+ * @author trela_1103395
+ */
+public class tulip extends flower{
+    
+}
