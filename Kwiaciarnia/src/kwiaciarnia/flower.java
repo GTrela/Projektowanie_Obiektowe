d36e8438dcb626eb561754a70761c0cfc7d03f76
trela_1103395 <trela_1103395@a05>
2017-10-02 13:17:14 +0200

    add client interface and flower class

---
+++ Kwiaciarnia/src/kwiaciarnia/flower.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kwiaciarnia;
+
+/**
+ *
+ * @author trela_1103395
+ */
+public class flower {
+    int id;
+    String name;
+    double value;
+}
