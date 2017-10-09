67f409939ebf0917055d5a35da5fd03833e8517b
Grzegorz Trela <grzegorz.trela@uj.edu.pl>
2017-10-09 08:58:59 +0200

    Changed flower to product class

--- Kwiaciarnia/src/kwiaciarnia/Product.java
+++ Kwiaciarnia/src/kwiaciarnia/Product.java
@@ -9,12 +9,12 @@ package kwiaciarnia;
  *
  * @author trela_1103395
  */
-public class flower {
+public class Product {
     int id;
     String name;
     double price;
     
-    public flower (int id, String name, double price) {
+    public Product(int id, String name, double price) {
         this.id = id;
         this.name = name;
         this.price = price;
