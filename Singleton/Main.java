d14ae88ec68a89840b7cda2aa4d74af1e7f8a589
Grzegorz <grzegorz.trela@uj.edu.pl>
2017-11-12 20:29:55 +0100

    delete unnecessary files

--- Singleton/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        Singleton tmp = Singleton.getInstance("Janusz","Kowalski",23);
-        System.out.println(tmp.getName()+" "+tmp.getSurname() +". Nr lotu:"+tmp.getLotNumber());
-        Singleton cos = Singleton.getInstance("Janusz","Kowalski",23);
-    }
-}
