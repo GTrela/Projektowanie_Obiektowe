be798fc99b7dc8e091ef013431723df4a070e807
manoftheoak <jakub.jas@student.uj.edu.pl>
2018-01-22 03:48:11 +0100

    + interface added

--- Geometry/src/ShapeRecognition.java
+++ Geometry/src/ShapeRecognition.java
@@ -5,6 +5,7 @@ import java.awt.image.Raster;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ShapeRecognition
 {
@@ -55,7 +56,7 @@ public class ShapeRecognition
 				}
 				else
 				{
-					System.out.printf("Relacja prostokątów nr %d i %d:\n", i, j);
+					System.out.printf("-- Relacja prostokątów nr %d i %d --\n", i, j);
 
 					boolean r1ContainsR2;
 					boolean r1TangencyR2;
@@ -83,7 +84,7 @@ public class ShapeRecognition
 
 					if (!r1ContainsR2 && !r1TangencyR2 && !r1SharePointsR2)
 					{
-						System.out.printf("Prostokąt %d jest rozłączna względem prostokąta %d\n", i, j);
+						System.out.printf("Prostokąt %d jest rozłączny względem prostokąta %d\n", i, j);
 					}
 
 					System.out.println();
@@ -106,7 +107,7 @@ public class ShapeRecognition
 				}
 				else
 				{
-					System.out.printf("Relacja elipsy nr %d i %d:\n", i, j);
+					System.out.printf("-- Relacja elipsy nr %d i %d --\n", i, j);
 
 					boolean r1ContainsR2;
 					boolean r1TangencyR2;
@@ -152,7 +153,7 @@ public class ShapeRecognition
 			for (int j = 0; j < er.detectedEllipses.size(); j++)
 			{
 
-				System.out.printf("Relacja prostokąta nr %d i elipsy %d:\n", i, j);
+				System.out.printf("-- Relacja prostokąta nr %d i elipsy %d --\n", i, j);
 
 				boolean r1ContainsR2;
 				boolean r1TangencyR2;
@@ -175,12 +176,12 @@ public class ShapeRecognition
 
 				if (r1SharePointsR2 = Position.R1SharePointsR2(er.detectedEllipses.get(j).getFramingRectangle(), rr.detectedRectangles.get(i)))
 				{
-					System.out.printf("Prostokąt %d ma część wspólną z elispą %d\n", i, j);
+					System.out.printf("Prostokąt %d ma część wspólną z elipsą %d\n", i, j);
 				}
 
 				if (!r1ContainsR2 && !r1TangencyR2 && !r1SharePointsR2)
 				{
-					System.out.printf("Prostokąt %d jest rozłączna względem elipsy %d\n", i, j);
+					System.out.printf("Prostokąt %d jest rozłączny względem elipsy %d\n", i, j);
 				}
 
 				System.out.println();
@@ -196,7 +197,7 @@ public class ShapeRecognition
 		{
 			for (int j = 0; j < rr.detectedRectangles.size(); j++)
 			{
-				System.out.printf("Relacja elipsy nr %d i prostokąta %d:\n", i, j);
+				System.out.printf("-- Relacja elipsy nr %d i prostokąta %d --\n", i, j);
 
 				boolean r1ContainsR2;
 				boolean r1TangencyR2;
@@ -214,7 +215,7 @@ public class ShapeRecognition
 
 				if (r1TangencyR2 = Position.R1TangencyR2(rr.detectedRectangles.get(j), er.detectedEllipses.get(i).getFramingRectangle()))
 				{
-					System.out.printf("Elipsa %d jest styczny do prostokąta %d\n", i, j);
+					System.out.printf("Elipsa %d jest styczna do prostokąta %d\n", i, j);
 				}
 
 				if (r1SharePointsR2 = Position.R1SharePointsR2(rr.detectedRectangles.get(j), er.detectedEllipses.get(i).getFramingRectangle()))
@@ -236,7 +237,7 @@ public class ShapeRecognition
 	{
 		if (args.length < 1)
 		{
-			System.out.println("Add file path as argument");
+			System.out.println("Podaj nazwę pliku jako argument");
 			System.exit(0);
 		}
 		else
@@ -244,6 +245,7 @@ public class ShapeRecognition
 			String imageFilePath = args[0];
 			try
 			{
+				Scanner scanner = new Scanner(System.in);
 				ShapeRecognition sr = new ShapeRecognition();
 				RectangleRecognition rr = new RectangleRecognition();
 				EllipseRecognition er = new EllipseRecognition();
@@ -258,22 +260,122 @@ public class ShapeRecognition
 				//detect every Ellipse
 				er.detectEllipses(imageRaster);
 
-				System.out.printf("Wykryto %d prostokąty:\n", rr.detectedRectangles.size());
-				for (int i = 0; i < rr.detectedRectangles.size(); i++)
-				{
-					System.out.printf("Prostokąt nr: %d\n%s\n", i, rr.detectedRectangles.get(i));
-				}
+				System.out.println();
 
-				System.out.printf("Wykryto %d elipsy:\n", er.detectedEllipses.size());
-				for (int i = 0; i < er.detectedEllipses.size(); i++)
-				{
-					System.out.printf("Elipsa nr: %d\n%s\n", i, er.detectedEllipses.get(i));
-				}
+				boolean exitFlag = false;
 
-				sr.printRectRectRelation(rr);
-				sr.printElipseElipseRelation(er);
-				sr.printRectangleElipseRelation(rr, er);
-				sr.printElipseRectangleRelation(er,rr);
+				while(!exitFlag)
+				{
+					System.out.println("------------------------------------------------");
+					System.out.printf("Wykryto %d prostokąty:\n", rr.detectedRectangles.size());
+					System.out.println("------------------------------------------------");
+					for (int i = 0; i < rr.detectedRectangles.size(); i++)
+					{
+						System.out.printf("Prostokąt nr: %d\n%s\n", i, rr.detectedRectangles.get(i));
+					}
+
+					System.out.println("------------------------------------------------");
+					System.out.printf("Wykryto %d elipsy:\n", er.detectedEllipses.size());
+					System.out.println("------------------------------------------------");
+					for (int i = 0; i < er.detectedEllipses.size(); i++)
+					{
+						System.out.printf("Elipsa nr: %d\n%s\n", i, er.detectedEllipses.get(i));
+					}
+
+					System.out.println("------------------------------------------------");
+					System.out.println("Wzajemne położenie figur");
+					System.out.println("------------------------------------------------");
+
+					sr.printRectRectRelation(rr);
+					sr.printElipseElipseRelation(er);
+					sr.printRectangleElipseRelation(rr, er);
+					sr.printElipseRectangleRelation(er,rr);
+
+					System.out.println("------------------------------------------------");
+					System.out.print("Przesunięcie o wektor [1], wyjście [2]: ");
+
+					boolean correctInput = false;
+					int option = 0;
+
+					do
+					{
+						if (scanner.hasNextInt())
+						{
+							option = scanner.nextInt();
+
+							if (option == 1 || option == 2)
+							{
+								correctInput = true;
+							}
+							else
+							{
+								System.out.print("Błędny numer opcji. Spróbuj ponownie: ");
+							}
+						}
+						else
+						{
+							System.out.print("Błędny numer opcji. Spróbuj ponownie: ");
+							scanner.next();
+						}
+					}
+					while(!correctInput);
+
+					if (option == 1)
+					{
+						System.out.print("Podaj współrzędne wektora (x, y) oddzielone spacją: ");
+						scanner.nextLine();
+						String input = scanner.nextLine();
+						String[] numbers = input.split(" ");
+
+						Point vector = new Point(Integer.parseInt(numbers[0]), Integer.parseInt(numbers[1]));
+
+						System.out.print("Czy chcesz przesunąć prostokąt(y)? T/N : ");
+						String answer = scanner.next();
+
+						if (answer.equals("T") || answer.equals("t"))
+						{
+							System.out.print("Podaj numery prostokątów do przesunięcia: ");
+							scanner.nextLine();
+							input = scanner.nextLine();
+							numbers = input.split(" ");
+
+							for (String number : numbers)
+							{
+								Rectangle rect = rr.detectedRectangles.get(Integer.parseInt(number));
+
+								if (rect != null)
+								{
+									rr.detectedRectangles.get(Integer.parseInt(number)).translateByVector(vector);
+								}
+							}
+						}
+
+						System.out.print("Czy chcesz przesunąć elipsę(y)? T/N : ");
+						answer = scanner.next();
+
+						if (answer.equals("T") || answer.equals("t"))
+						{
+							System.out.print("Podaj numery elips do przesunięcia: ");
+							scanner.nextLine();
+							input = scanner.nextLine();
+							numbers = input.split(" ");
+
+							for (String number : numbers)
+							{
+								Ellipse ellipse = er.detectedEllipses.get(Integer.parseInt(number));
+
+								if (ellipse != null)
+								{
+									er.detectedEllipses.get(Integer.parseInt(number)).translateByVector(vector);
+								}
+							}
+						}
+					}
+					else
+					{
+						exitFlag = true;
+					}
+				}
 			}
 
 			catch (IOException e)
