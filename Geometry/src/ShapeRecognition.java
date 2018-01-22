93a14324b40a018a5fda952248348ab4fa1a0822
Grzegorz Trela <grzegorz.trela@uj.edu.pl>
2018-01-22 02:34:55 +0100

    Add printing rectangle ellipse relations

--- Geometry/src/ShapeRecognition.java
+++ Geometry/src/ShapeRecognition.java
@@ -5,7 +5,6 @@ import java.awt.image.Raster;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class ShapeRecognition
 {
@@ -42,6 +41,197 @@ public class ShapeRecognition
 		}
 	}
 
+	public void printRectRectRelation(RectangleRecognition rr)
+	{
+		// rectangle vs rectangle relations
+		for (int i = 0; i < rr.detectedRectangles.size(); i++)
+		{
+			for (int j = 0; j < rr.detectedRectangles.size(); j++)
+			{
+				// if rectangle is the same object
+				if (rr.detectedRectangles.get(i) == rr.detectedRectangles.get(j))
+				{
+					continue;
+				}
+				else
+				{
+					System.out.printf("Relacja prostokątów nr %d i %d:\n", i, j);
+
+					boolean r1ContainsR2;
+					boolean r1TangencyR2;
+					boolean r1SharePointsR2;
+
+					if (Position.R1InsideR2(rr.detectedRectangles.get(j), rr.detectedRectangles.get(i)))
+					{
+						System.out.printf("Prostokąt %d znajduje się wewnątrz prostokąta %d\n", i, j);
+					}
+
+					if (r1ContainsR2 = Position.R1ContainsR2(rr.detectedRectangles.get(j), rr.detectedRectangles.get(i)))
+					{
+						System.out.printf("Prostokąt %d zawiera się w prostokącie %d\n", i, j);
+					}
+
+					if (r1TangencyR2 = Position.R1TangencyR2(rr.detectedRectangles.get(j), rr.detectedRectangles.get(i)))
+					{
+						System.out.printf("Prostokąt %d jest styczny do prostokąta %d\n", i, j);
+					}
+
+					if (r1SharePointsR2 = Position.R1SharePointsR2(rr.detectedRectangles.get(j), rr.detectedRectangles.get(i)))
+					{
+						System.out.printf("Prostokąt %d ma część wspólną z prostokątem %d\n", i, j);
+					}
+
+					if (!r1ContainsR2 && !r1TangencyR2 && !r1SharePointsR2)
+					{
+						System.out.printf("Prostokąt %d jest rozłączna względem prostokąta %d\n", i, j);
+					}
+
+					System.out.println();
+				}
+			}
+		}
+	}
+
+	public void printElipseElipseRelation(EllipseRecognition er)
+	{
+		//ellipse vs ellipse relations
+		for (int i = 0; i < er.detectedEllipses.size(); i++)
+		{
+			for (int j = 0; j < er.detectedEllipses.size(); j++)
+			{
+				// if elipse is the same object
+				if (er.detectedEllipses.get(i) == er.detectedEllipses.get(j))
+				{
+					continue;
+				}
+				else
+				{
+					System.out.printf("Relacja elipsy nr %d i %d:\n", i, j);
+
+					boolean r1ContainsR2;
+					boolean r1TangencyR2;
+					boolean r1SharePointsR2;
+
+					if (Position.R1InsideR2(er.detectedEllipses.get(j).getFramingRectangle(), er.detectedEllipses.get(i).getFramingRectangle()))
+					{
+						System.out.printf("Elipsa %d znajduje się wewnątrz elipsy %d\n", i, j);
+					}
+
+					if (r1ContainsR2 = Position.R1ContainsR2(er.detectedEllipses.get(j).getFramingRectangle(), er.detectedEllipses.get(i).getFramingRectangle()))
+					{
+						System.out.printf("Elipsa %d zawiera się w elipsie %d\n", i, j);
+					}
+
+					if (r1TangencyR2 = Position.R1TangencyR2(er.detectedEllipses.get(j).getFramingRectangle(), er.detectedEllipses.get(i).getFramingRectangle()))
+					{
+						System.out.printf("Elipsa %d jest styczna do elipsy %d\n", i, j);
+					}
+
+					if (r1SharePointsR2 = Position.R1SharePointsR2(er.detectedEllipses.get(j).getFramingRectangle(), er.detectedEllipses.get(i).getFramingRectangle()))
+					{
+						System.out.printf("Elipsa %d ma część wspólną z elipsą %d\n", i, j);
+					}
+
+					if (!r1ContainsR2 && !r1TangencyR2 && !r1SharePointsR2)
+					{
+						System.out.printf("Elipsa %d jest rozłączna względem elipsy %d\n", i, j);
+					}
+
+					System.out.println();
+				}
+			}
+		}
+	}
+
+
+	public void printRectangleElipseRelation(RectangleRecognition rr, EllipseRecognition er)
+	{
+		//rectangle vs ellipse relations
+		for (int i = 0; i < rr.detectedRectangles.size(); i++)
+		{
+			for (int j = 0; j < er.detectedEllipses.size(); j++)
+			{
+
+				System.out.printf("Relacja prostokąta nr %d i elipsy %d:\n", i, j);
+
+				boolean r1ContainsR2;
+				boolean r1TangencyR2;
+				boolean r1SharePointsR2;
+
+				if (Position.R1InsideR2(er.detectedEllipses.get(j).getFramingRectangle(), rr.detectedRectangles.get(i)))
+				{
+					System.out.printf("Prostokąt %d znajduje się wewnątrz elipsy %d\n", i, j);
+				}
+
+				if (r1ContainsR2 = Position.R1ContainsR2(er.detectedEllipses.get(j).getFramingRectangle(), rr.detectedRectangles.get(i)))
+				{
+					System.out.printf("Prostokąt %d zawiera się w elipsie %d\n", i, j);
+				}
+
+				if (r1TangencyR2 = Position.R1TangencyR2(er.detectedEllipses.get(j).getFramingRectangle(), rr.detectedRectangles.get(i)))
+				{
+					System.out.printf("Prostokąt %d jest styczny do elipsy %d\n", i, j);
+				}
+
+				if (r1SharePointsR2 = Position.R1SharePointsR2(er.detectedEllipses.get(j).getFramingRectangle(), rr.detectedRectangles.get(i)))
+				{
+					System.out.printf("Prostokąt %d ma część wspólną z elispą %d\n", i, j);
+				}
+
+				if (!r1ContainsR2 && !r1TangencyR2 && !r1SharePointsR2)
+				{
+					System.out.printf("Prostokąt %d jest rozłączna względem elipsy %d\n", i, j);
+				}
+
+				System.out.println();
+
+			}
+		}
+	}
+
+
+	public void printElipseRectangleRelation(EllipseRecognition er, RectangleRecognition rr)
+	{
+		for (int i = 0; i < er.detectedEllipses.size(); i++)
+		{
+			for (int j = 0; j < rr.detectedRectangles.size(); j++)
+			{
+				System.out.printf("Relacja elipsy nr %d i prostokąta %d:\n", i, j);
+
+				boolean r1ContainsR2;
+				boolean r1TangencyR2;
+				boolean r1SharePointsR2;
+
+				if (Position.R1InsideR2(rr.detectedRectangles.get(j), er.detectedEllipses.get(i).getFramingRectangle()))
+				{
+					System.out.printf("Elipsa %d znajduje się wewnątrz prostokąta %d\n", i, j);
+				}
+
+				if (r1ContainsR2 = Position.R1ContainsR2(rr.detectedRectangles.get(j), er.detectedEllipses.get(i).getFramingRectangle()))
+				{
+					System.out.printf("Elipsa %d zawiera się w prostokącie %d\n", i, j);
+				}
+
+				if (r1TangencyR2 = Position.R1TangencyR2(rr.detectedRectangles.get(j), er.detectedEllipses.get(i).getFramingRectangle()))
+				{
+					System.out.printf("Elipsa %d jest styczny do prostokąta %d\n", i, j);
+				}
+
+				if (r1SharePointsR2 = Position.R1SharePointsR2(rr.detectedRectangles.get(j), er.detectedEllipses.get(i).getFramingRectangle()))
+				{
+					System.out.printf("Elipsa %d ma część wspólną z prostokątem %d\n", i, j);
+				}
+
+				if (!r1ContainsR2 && !r1TangencyR2 && !r1SharePointsR2)
+				{
+					System.out.printf("Elipsa %d jest rozłączna względem prostokąta %d\n", i, j);
+				}
+
+				System.out.println();
+			}
+		}
+	}
+
 	public static void main(String[] args)
 	{
 		if (args.length < 1)
@@ -68,9 +258,22 @@ public class ShapeRecognition
 				//detect every Ellipse
 				er.detectEllipses(imageRaster);
 
-				System.out.println(er.getDetectedEllipses().get(0).toString());
-				System.out.println(rr.getDetectedRectangles().get(0).toString());
+				System.out.printf("Wykryto %d prostokąty:\n", rr.detectedRectangles.size());
+				for (int i = 0; i < rr.detectedRectangles.size(); i++)
+				{
+					System.out.printf("Prostokąt nr: %d\n%s\n", i, rr.detectedRectangles.get(i));
+				}
 
+				System.out.printf("Wykryto %d elipsy:\n", er.detectedEllipses.size());
+				for (int i = 0; i < er.detectedEllipses.size(); i++)
+				{
+					System.out.printf("Elipsa nr: %d\n%s\n", i, er.detectedEllipses.get(i));
+				}
+
+				sr.printRectRectRelation(rr);
+				sr.printElipseElipseRelation(er);
+				sr.printRectangleElipseRelation(rr, er);
+				sr.printElipseRectangleRelation(er,rr);
 			}
 
 			catch (IOException e)
